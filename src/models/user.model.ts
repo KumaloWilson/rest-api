a6c52c7263b4a6208fa955e9a0c1be3bb2f315d3
KumaloWilson <kumalowilson900@gmail.com>
2025-07-15 21:43:40 +0200

    video player fixeds

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,42 +1,35 @@
-import { Column, DataType, Model, Table, } from "sequelize-typescript";
+import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 @Table({
-    tableName: 'users',
-    timestamps: true,
+  tableName: "users",
+  timestamps: true, // This automatically adds createdAt and updatedAt
 })
+export class User extends Model<User> {
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id!: number;
 
-export class User extends Model {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+  })
+  name!: string;
 
-    @Column(
-        {
-            type: DataType.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        }
-    )
-    id!: number;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,  // You probably want to enforce this
+    unique: true,
+  })
+  email!: string;
 
-
-    @Column({
-        type: DataType.STRING,
-        allowNull: false,
-        unique: true,
-    })
-    name!: string;
-
-
-    @Column(DataType.STRING)
-    email!: string;
-
-
-    @Column(DataType.NUMBER)
-    age!: number;
-
-
-    @Column(DataType.DATE)
-    createdAt!: Date;
-
-    @Column(DataType.DATE)
-    updatedAt!: Date;
+  @Column({
+    type: DataType.INTEGER, // Use INTEGER for numeric columns
+    allowNull: false,
+  })
+  age!: number;
 
 }
